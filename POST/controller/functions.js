ec8e47b92e9b23dbd9b31ee6449d6f195ef18806
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-03-06 19:14:05 +0100

    fix the input names +make edit page for article

--- POST/controller/functions.js
+++ POST/controller/functions.js
@@ -11,33 +11,7 @@ const querystring = require('querystring');
 
 function addart(req, res) {
     if (req.method === "POST") {
-        let body = '';
-        
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-
-        req.on('end', () => {
-            const formData = querystring.parse(body);
-            const Title = formData.Title;
-            const Article = formData.Article;
-            
-            const article = new Articleblog({
-                Title: Title,
-                Article: Article
-            });
-            console.log(article);
-            
-            article.save()
-                .then(() => Articleblog.find())
-                .then(articles => {
-                    res.render("arti", { articles, A: true });
-                })
-                .catch(error => {
-                    console.error("Error adding article:", error);
-                    res.status(500).send("Error adding article");
-                });
-        });
+        GetSavedata()
     } else if (req.method === "GET") {
         res.render("addartical");
     }
@@ -71,9 +45,43 @@ const artedit=((req,res)=>{
     Articleblog.findById(flag)
                .then(result=>res.render("edit",{ article:result }))
     }else if(req.method === "POST"){
-        res.render("arti");
+      GetSavedata(req,res)
+}})
+
+
+
+
+function GetSavedata(req,res) {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const formData = querystring.parse(body);
+            const Title = formData.Title;
+            const Article = formData.Article;
+
+            const article = new Articleblog({
+                Title: Title,
+                Article: Article
+            });
+            console.log(article);
+
+            article.save()
+                .then(() => Articleblog.find())
+                .then(articles => {
+                    res.render("arti", { articles, A: true });
+                })
+                .catch(error => {
+                    console.error("Error adding article:", error);
+                    res.status(500).send("Error adding article");
+        })
+        })
     }
-})
+
+
 
 module.exports={
 art,
