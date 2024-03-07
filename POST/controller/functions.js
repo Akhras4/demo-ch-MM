b3fa862bedd87a8608d10a3f09cc80399cc56b28
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-03-07 10:05:40 +0100

    update

--- POST/controller/functions.js
+++ POST/controller/functions.js
@@ -1,57 +1,14 @@
 const { model } = require('mongoose');
 const Articleblog=require("../modules/db")
-
-const art=((req,res)=>{
-    Articleblog.find()
-               .then((result)=> res.render("arti",{ articles:result ,A:true}))
-    
-})
 const querystring = require('querystring');
 
+const art = ((req, res) => {
+    Articleblog.find()
+        .then((result) => res.render("arti", { articles: result, A: true }))
 
+})
 function addart(req, res) {
     if (req.method === "POST") {
-        GetSavedata(req, res)
-    } else if (req.method === "GET") {
-        res.render("addartical");
-    }
-}
-var flag
-const artId=((req,res)=>{
-    var articleId = req.params.id;
-    flag=articleId;
-     Articleblog.findById(articleId)
-    .then(article => {
-    if (!article) {
-        return res.status(404).send("Article not found");
-    }
-    console.log(article);
-    res.render("showart",{article})
-    })
-})
-
-const artIddel=((req,res)=>{
-        Articleblog.deleteOne({ _id: flag})
-        .then(()=>{
-            Articleblog.find()
-            .then(result=>res.render("arti",{articles:result ,A:true}))
-            .catch(err=>console.log(err))
-        })
-        .catch(err=>console.log(err))
-
-})
-const artedit=((req,res)=>{
-    if (req.method === "GET") {
-    Articleblog.findById(flag)
-               .then(result=>res.render("edit",{ article:result }))
-    }else if(req.method === "POST"){
-      GetSavedata(req,res)
-}})
-
-
-
-
-function GetSavedata(req,res) {
         let body = '';
 
         req.on('data', chunk => {
@@ -77,12 +34,64 @@ function GetSavedata(req,res) {
                 .catch(error => {
                     console.error("Error adding article:", error);
                     res.status(500).send("Error adding article");
+                })
         })
+    } else if (req.method === "GET") {
+        res.render("addartical");
+    }
+}
+var flag
+const artId = ((req, res) => {
+    var articleId = req.params.id;
+    flag = articleId;
+    Articleblog.findById(articleId)
+        .then(article => {
+            if (!article) {
+                return res.status(404).send("Article not found");
+            }
+            console.log(article);
+            res.render("showart", { article })
+        })
+})
+
+const artIddel = ((req, res) => {
+    Articleblog.deleteOne({ _id: flag })
+        .then(() => {
+            Articleblog.find()
+                .then(result => res.render("arti", { articles: result, A: true }))
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+
+})
+const artedit = ((req, res) => {
+    if (req.method === "GET") {
+        Articleblog.findById(flag)
+            .then(result => res.render("edit", { article: result }))
+    } else if (req.method === "POST") {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const formData = querystring.parse(body);
+            const Title = formData.Title;
+            const Article = formData.Article;
+            Articleblog.findByIdAndUpdate(flag, { Title, Article })
+                .then(() => Articleblog.find())
+                .then(articles => {
+                    res.render("arti", { articles, A: true });
+                })
+                .catch(error => {
+                    console.error("Error adding article:", error);
+                    res.status(500).send("Error adding article");
+                })
         })
     }
 
-
-
+})
 module.exports={
 art,
 addart,
